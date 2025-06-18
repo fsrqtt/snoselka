4aa74ee002c07aa835bd6b2a1785df7f63126f99
fsrqtt <gollartem2@gmail.com>
2025-06-18 23:07:08 +0300

    server.py

--- server.py
+++ server.py
@@ -1,13 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)  # разрешаем CORS-запросы (для HTML страницы)
 
 USERS = {
     "admin": "789456123",
     "Skizzy": "Skizzy13371337",
     "cultionpanic": "789456123"
 }
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -19,7 +25,7 @@ def login():
     if username in USERS and USERS[username] == password:
         return jsonify({"status": "ok", "message": f"Добро пожаловать, {username}!"}), 200
     else:
-        return jsonify({"status": "error", "message": "Неверный логин или пароль, напишите @cultionpanic или @tenevoypanic для выяснения обстоятельств"}), 401
+        return jsonify({"status": "error", "message": "Неверный логин или пароль, напишите @cultionpanic или @tenevoypanic"}), 401
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
